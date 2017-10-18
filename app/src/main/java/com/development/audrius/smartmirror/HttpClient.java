c39b68f289b7f26c8a385dde3ef71a751618fb63
Audrius <audriusa83@gmail.com>
2017-10-18 23:46:05 +0100

    Added logic to call MetOffice

---
+++ app/src/main/java/com/development/audrius/smartmirror/HttpClient.java
@@ -0,0 +1,8 @@
+package com.development.audrius.smartmirror;
+
+/**
+ * Created by Audri on 18/10/2017.
+ */
+
+class HttpClient {
+}
---
+++ app/src/main/java/com/development/audrius/smartmirror/MetService.java
@@ -0,0 +1,9 @@
+package com.development.audrius.smartmirror;
+
+/**
+ * Created by Audri on 17/10/2017.
+ */
+
+public class MeteoService {
+    
+}
